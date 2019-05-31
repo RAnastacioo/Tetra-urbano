24c8fd28da9b20a727b1a5a25bf308a2e353cdac
RicardoAnastacio <RicardoAnastacio@github.com>
2019-05-31 13:56:26 +0100

    31/05/2019-13.56
    31/05/2019-13.56

--- Main.m
+++ Main.m
@@ -1,31 +1,29 @@
 clearvars;clc;close all;
 
 SAMPLES = 512;
-% load('backup_Lisboa_512.mat')
-load('backup_Porto_512.mat')
+%load('backup_Lisboa_512.mat')
+%load('backup_Porto_512.mat')
+load('backup_512_new.mat')
 load('Antena400MhzGain13.mat');
 disp('Displaying Data');
-
-%%------- Max Elevation Point
-[~,index] = max(elevation_map(:));
-% maxElevation=[lng_map(256,256),lat_map(256,256),elevation_map(256,256)];
-maxElevation=[lng_map(index),lat_map(index),elevation_map(index)];
-
-%%------- Points
-points = maxElevation;
-points(2,:)=[-8.41172240000000,41.0963930000000,394.334533700000];
-points(3,:)=[-8.58700970000000,41.1077348000000,232.853225700000];
-
 %% All Line-of-sight visibility points in terrain
 latlim = [min(lat_map(:)), max(lat_map(:))];
 lonlim = [min(lng_map(:)), max(lng_map(:))];
 rasterSize = size(elevation_map);
 %GEOREFCELLS Reference raster cells to geographic coordinates
-R = georefcells(latlim,lonlim,rasterSize,'ColumnsStartFrom','north');
+R = georefpostings(latlim,lonlim,rasterSize,'ColumnsStartFrom','north');
 
 %% BBC
-% [visgrid] = bestBsCoverage(elevation_map,lat_map,lng_map,R);
+bestBsCoverage(elevation_map,lat_map,lng_map,R);
 
+%%------- Max Elevation Point
+[~,index] = max(elevation_map(:));
+maxElevation=[lng_map(index),lat_map(index),elevation_map(index)];
+%%------- Points
+ points = maxElevation;
+% points = [-7.921640300000000,41.292964000000000,1.190174926800000e+03];
+points(2,:)=[-8.031364700000000,41.194099000000000,9.509163818000000e+02];
+points(3,:)=[-8.236043900000000,41.187896000000000,5.677875366000000e+02];
 %% BS1
 [Prx_dBmBS1,visgridBS1]=Antena('BS1','Coverage Map - BS1',points(1,1),points(1,2),points(1,3),elevation_map,lat_map,lng_map,R);
 %% BS2
@@ -44,12 +42,15 @@ Prx_dBm(visgridBS3)=Prx_dBmBS3(visgridBS3);
 Sub=and(visgridBS1,visgridBS2);
 Sub2=and(visgridBS1,visgridBS3);
 Sub3=and(visgridBS2,visgridBS3);
+
 %% color devision
 signalColor=colorLegend(Prx_dBm);
 % mesh(lng_map(1,:), lat_map(:,1), elevation_map,signalColor);
+
 %% Displays the data
 figure('Name','BS1+BS2+BS3');
-subplot(1,2,1);
+%subplot(1,2,1);
+axis tight
 mesh(lng_map(1,:), lat_map(:,1), elevation_map,signalColor);
 hold on
 title('Coverage Map - BS1 & BS2 & BS3');
@@ -59,13 +60,11 @@ zlabel('Elevation (m)');
 scatter3(points(1,1),points(1,2),points(1,3),'filled','v','r','SizeData',200);
 scatter3(points(2,1),points(2,2),points(2,3),'filled','v','r','SizeData',200);
 scatter3(points(3,1),points(3,2),points(3,3),'filled','v','r','SizeData',200);
-plot3(lng_map(Sub),lat_map(Sub),elevation_map(Sub),'w.','markersize',5);
 plot3(lng_map(Sub2),lat_map(Sub2),elevation_map(Sub2),'w.','markersize',5);
 plot3(lng_map(Sub3),lat_map(Sub3),elevation_map(Sub3),'w.','markersize',5);
 hold off
-subplot(1,2,2);
-imshow('z_Legend.jpg');
-
+%subplot(1,2,2);
+%imshow('z_Legend.jpg');
 
 %% Antenna Patern Atenuação 3d
 load('Antena400MhzGain13.mat');
--- PL_Hata_modify.m
+++ PL_Hata_modify.m
@@ -12,12 +12,11 @@ function PL=PL_Hata(fc,d,htx,hrx,Etype)
 %MIMO-OFDM Wireless Communications with MATLAB¢ç   Yong Soo Cho, Jaekwon Kim, Won Young Yang and Chung G. Kang
 %2010 John Wiley & Sons (Asia) Pte Ltd
 
-%modify RA
 if nargin<5, Etype = 'URBAN'; end
 fc=fc/(1e6);
-if fc>=150&&fc<=200, C_Rx = 8.29.*(log10(1.54.*hrx)).^2 - 1.1;
- elseif fc>200, C_Rx = 3.2.*(log10(11.75.*hrx)).^2 - 4.97; % Eq. (1.9)
- else   C_Rx = 0.8+(1.1.*log10(fc)-0.7).*hrx-1.56.*log10(fc); % Eq. (1.8)
+if fc>=150&&fc<=200, C_Rx = 8.29.*(log10(1.54*hrx)).^2 - 1.1;
+ elseif fc>200, C_Rx = 3.2.*(log10(11.75*hrx)).^2 - 4.97; % Eq. (1.9)
+ else   C_Rx = 0.8+(1.1.*log10(fc)-0.7)*hrx-1.56.*log10(fc); % Eq. (1.8)
 end
 PL = 69.55 +26.16.*log10(fc) -13.82.*log10(htx) -C_Rx ...
      +(44.9-6.55.*log10(htx)).*log10(d./1000);  % Eq. (1.7)
